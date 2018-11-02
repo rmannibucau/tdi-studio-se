81e9252f561f3fd00eeab736c825baf5c516257f
Ivan Honchar <ivan.honchar@globallogic.com>
2018-11-02 14:48:57 +0200

    fix(TUP-20926): move metadata retrieval from SchemaActionParameter to SchemaElementParameter

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/SchemaElementParameter.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/SchemaElementParameter.java
@@ -2,9 +2,16 @@ package org.talend.sdk.component.studio.model.parameter;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.talend.core.model.metadata.IMetadataColumn;
+import org.talend.core.model.metadata.IMetadataTable;
+import org.talend.core.model.process.IConnection;
 import org.talend.core.model.process.IElement;
+import org.talend.core.model.utils.NodeUtil;
+import org.talend.core.runtime.IAdditionalInfo;
+import org.talend.designer.core.ui.editor.nodes.Node;
 import org.talend.sdk.component.studio.model.action.IActionParameter;
 
 /**
@@ -12,9 +19,7 @@ import org.talend.sdk.component.studio.model.action.IActionParameter;
  */
 public class SchemaElementParameter extends TaCoKitElementParameter {
 
-    public SchemaElementParameter() {
-        super();
-    }
+    public static final String CONNECTION_TYPE = "org.talend.sdk.connection.type";
 
     public SchemaElementParameter(final IElement element) {
         super(element);
@@ -32,7 +37,71 @@ public class SchemaElementParameter extends TaCoKitElementParameter {
      */
     @Override
     public List<String> getValue() {
-        return Collections.emptyList(); //TODO
+        final Optional<IMetadataTable> metadata = getMetadata();
+        if (metadata.isPresent()) {
+            return toSchema(metadata.get());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    private Optional<IMetadataTable> getMetadata() {
+        IElement elem = getElement();
+        if (elem == null || !(elem instanceof Node)) {
+            return Optional.empty();
+        }
+        // TODO connection type should be set in constructor and should be always present
+        final String connectionType = (String) getTaggedValue(CONNECTION_TYPE);
+        if (connectionType == null) {
+            return Optional.empty();
+        }
+        final Optional<IConnection> connection = findConnection(connectionType, (Node) elem);
+        return connection.map(IConnection::getMetadataTable);
+
+    }
+
+    // TODO try to get Metadata directly form Node, not from Connection
+    private Optional<IConnection> findConnection(final String connectionType, final Node node) {
+        if ("in".equalsIgnoreCase(connectionType)) {
+            if (node.getComponent().useLookup()) {
+                for (final IConnection conn : node.getIncomingConnections()) {
+                    if (!(conn instanceof IAdditionalInfo)) {
+                        continue;
+                    }
+                    String inputName = (String) IAdditionalInfo.class.cast(conn).getInfo("INPUT_NAME");
+                    if (inputName != null && inputName.equals(getContext())) {
+                        return Optional.of(conn);
+                    }
+                }
+            } else {
+                final List<? extends IConnection> connections = NodeUtil.getIncomingConnections(node, getContext());
+                if (connections != null && !connections.isEmpty()) {
+                    return Optional.of(connections.get(0));
+                }
+            }
+        } else {
+            final List<? extends IConnection> connections = NodeUtil.getOutgoingConnections(node, getContext());
+            if (connections != null && !connections.isEmpty()) {
+                return Optional.of(connections.get(0));
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Retrieves schema column names {code List<String>} from IMetadataTable
+     *
+     * @param metadata IMetadataTable which represents schema
+     * @return {code List<String>} of schema column names
+     */
+    private List<String> toSchema(final IMetadataTable metadata) {
+        final List<IMetadataColumn> columns = metadata.getListColumns();
+        if (columns == null) {
+            return Collections.emptyList();
+        }
+        return columns.stream()
+                .map(IMetadataColumn::getLabel)
+                .collect(Collectors.toList());
     }
 
     /**
